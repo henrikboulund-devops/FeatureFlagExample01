8f4038ac106069bc0f23ac07738def77a635a984
Henrik Boulund <>
2023-02-24 11:29:10 +0100

    Init

---
+++ FeatureFlagExample01/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FeatureFlagExample01.Models;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace FeatureFlagExample01.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    [FeatureGate("NewFeatureFlag")]
+    public IActionResult NewFeature()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
